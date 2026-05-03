9c37d25a46fe09ddf55189650ad0388ad7295396
Golam Wasy Arnob <golamwasy@gmail.com>
2026-05-04 01:15:06 +0300

    removed planora link

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,6 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/projects/planora/:path*',
-        destination: 'https://planora-rose.vercel.app/:path*',
-      },
-      {
-        source: '/api/:path*',
-        destination: 'https://planora-rose.vercel.app/api/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
